a6bd1142562b6041bcacac025731b0693b7c6d40
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2024-11-21 16:50:55 -0300

    nAV E js

--- meuscript.js
+++ meuscript.js
@@ -17,5 +17,29 @@ window.addEventListener("scroll", function () {
     image.style.transition = "height 0.3s ease";
 });
 
+/* abaixo menu com overlay versao mobile*/ 
+
+let btnMenu = document.getElementById('btn-abrir-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
 
 
+/* AQUI PARA MUDAR A COR */
+const changeThemeBtn =  document.querySelector("#change-theme")
+
+changeThemeBtn.addEventListener("change", function() {
+    document.body.classList.toggle("dark");
+
+})
