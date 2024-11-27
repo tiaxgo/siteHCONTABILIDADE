9c37f40a99a4de119da91d0f378ae0f62d4ee21d
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2024-11-27 12:21:22 -0300

    up

--- meuscript.js
+++ meuscript.js
@@ -5,17 +5,38 @@ window.addEventListener("scroll", function () {
     // Verifica se a largura da tela é menor ou igual a 768px (versão mobile)
     if (window.matchMedia("(max-width: 1000px)").matches) {
         // Para mobile: A altura será ajustada entre 0 e um valor fixo (exemplo: 30vh)
-        let newHeight = Math.max(20 - scrollPosition / 20, 0); // Limita o mínimo em 0vh
+        let newHeight = Math.max(29 - scrollPosition / 10, 0); // Limita o mínimo em 0vh
         image.style.height = `${newHeight}vh`;
     } else {
         // Para desktop: Mantém a lógica original
-        let newHeight = Math.max(37 - scrollPosition / 30, 30); // Limita o mínimo em 20vh
+        let newHeight = Math.max(30 - scrollPosition / 10, 10); // Limita o mínimo em 20vh
         image.style.height = `${newHeight}vh`;
     }
 
     // Adiciona uma transição suave
-    image.style.transition = "height 0.2s ease";
+    image.style.transition = "height 0.3s ease";
 });
+
+window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY; // Posição de rolagem
+    let image = document.querySelector(".image-container-servicos");
+
+    // Verifica se a largura da tela é menor ou igual a 768px (versão mobile)
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+        // Para mobile: A altura será ajustada entre 0 e um valor fixo (exemplo: 30vh)
+        let newHeight = Math.max(30 - scrollPosition / 10, 0); // Limita o mínimo em 0vh
+        image.style.height = `${newHeight}vh`;
+    } else {
+        // Para desktop: Mantém a lógica original
+        let newHeight = Math.max(60 - scrollPosition / 10, 10); // Limita o mínimo em 20vh
+        image.style.height = `${newHeight}vh`;
+    }
+
+    // Adiciona uma transição suave
+    image.style.transition = "height 0.3s ease";
+});
+
+
 
 /* abaixo menu com overlay versao mobile*/ 
 
